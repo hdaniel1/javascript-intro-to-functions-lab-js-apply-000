0e144d049edf128f70fde898a83ed2d522f98619
Daniel Hallada <hallada.daniel@gmail.com>
2018-12-17 03:47:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,3 +13,11 @@ function logShout(string) {
 function logWhisper(string) {
   console.log(string.toLowerCase())
 }
+
+function sayHiToGrandma(string) {
+  var upper = string.toUpperCase()
+  if (string === upper) {
+    return 'I can\'t hear you!'
+  }
+
+  }
